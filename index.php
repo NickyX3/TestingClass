7427ddbc5867248074b39de9b3290b2905470048
Nic Latyshev <nickyx3@gmail.com>
2015-12-04 16:11:44 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 	error_reporting(E_ERROR | E_WARNING);
 	setlocale(LC_TIME,'ru_RU.UTF-8');
-	
+	// autoloader :-)
 	include_once 'cloader.php';
 	
 	$results = array();
 	$am 	= new TestClass();
-	
+	// for total time
 	$c		= new Bench();
-	
+	// 10 iterations for averages
 	for($a=1;$a<=10;$a++) {
+		// for one run bench
 		$b	= new Bench();
 		for($i=1;$i<=100000;$i++) {
 			$nav 	= $am->generateMenu();
